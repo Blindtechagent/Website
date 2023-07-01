b3c0644b0e42840e8b4bf8222aa0971777545fa1
Blind tech agent <pawanpianoartist@gmail.com>
2023-07-01 12:23:40 +0530

    Add files via upload

--- signup.js
+++ signup.js
@@ -19,7 +19,7 @@ form.addEventListener('submit', (e) => {
       const userName = document.getElementById("name").value;
 
       // Save the user's name to the database using the email ID as the key
-      firebase.database().ref("users/" + emailId).set({
+      firebase.database().ref("users/" + emailId.replace('.', ',')).set({
         name: userName
       })
       .then(() => {

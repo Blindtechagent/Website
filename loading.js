ac3ef300a1cd8822974793de147097aa64875337
Blind tech agent <pawanpianoartist@gmail.com>
2023-07-18 19:22:39 +0530

    Add files via upload

--- loading.js
+++ loading.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function() {
       setTimeout(function() {
         var preloader = document.querySelector('.preloader');
-        var body = document.querySelector('#body');
+        var body = document.querySelector('.body');
         preloader.remove(); // Remove the preloader element from the document
 body.style.display = "block";
       }, 2500);
